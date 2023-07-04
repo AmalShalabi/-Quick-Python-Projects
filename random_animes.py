95984c61f2bf0db7eacda00464a8e97f53480fab
unknown <mouni.shalaby@gmail.com>
2023-07-04 13:34:46 +0300

    spelling checker app

---
+++ random_animes.py
@@ -0,0 +1,7 @@
+from random import choice
+
+#creat lit of animes 
+animes=['naruto', 'haikyuu', 'mushishi', 'march comes in like a lion','ruto']
+
+#print a random anime from animes
+print(choice(animes))
