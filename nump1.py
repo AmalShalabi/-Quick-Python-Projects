a0472d28fd62816df3c106376e87018e976bcf15
unknown <mouni.shalaby@gmail.com>
2023-07-06 12:53:13 +0300

    create array in numpy

--- nump1.py
+++ nump1.py
@@ -2,18 +2,18 @@ import numpy as np
 my_list=[1,2,3,4,5,6]
 my_array=np.array(my_list)
 
-print(my_list)
-print(my_array)
+print(my_list) #[1,2,3,4,5,6]
+print(my_array) #[1 2 3 4 5 6]
 
 #Type
 print("#"*50)
-print(type(my_list))
-print(type(my_array))
+print(type(my_list)) # class 'list'
+print(type(my_array)) # class 'nympy.ndarray'
 
 #Accessing Elements 
 print("#"*50)
-print(my_list[0])
-print(my_array[0])
+print(my_list[0]) #1
+print(my_array[0]) #1
 
 print("#"*50)
 a=np.array(10)
@@ -21,18 +21,18 @@ b=np.array([10,20])
 c=np.array([[1,2],[3,4]])
 d=np.array([[[5,6],[7,9]],[[1,3],[4,8]]])
 
-print(d[1][1][1])
-print(d[1,1,1])
-print(d[1,1,-1])
+print(d[1][1][1]) # 8
+print(d[1,1,1]) # 8
+print(d[1,1,-1]) # 8
 
 print("#"*50)
 
-print(a.ndim)
-print(b.ndim)
-print(c.ndim)
-print(d.ndim)
+print(a.ndim) # 0
+print(b.ndim) # 1
+print(c.ndim) # 2
+print(d.ndim) # 3
 
 print("#"*50)
 # Custom Dimenstions
 my_custom_array=np.array([1,2,3],ndmin=3)
-print(my_custom_array[0][0][0])
+print(my_custom_array[0][0][0]) # 1
